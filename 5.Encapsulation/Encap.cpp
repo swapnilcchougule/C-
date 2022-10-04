572e8c64e49976ad14fb769fa163d049ce69e6a0
swapnilcchougule <swapnilcchougule@gmail.com>
2022-10-04 16:25:16 +0200

    Encapsulation code added

--- 5.Encapsulation/Encap.cpp
+++ 5.Encapsulation/Encap.cpp
@@ -1,3 +1,27 @@
 /*Encapsulation, is to make sure that "sensitive" data is hidden from users. 
 To achieve this, you must declare class variables/attributes as private (cannot be accessed from outside the class). 
-If you want others to read or modify the value of a private member, you can provide publicget and set methods.*/ 
+If you want others to read or modify the value of a private member, you can provide public get and set methods.*/ 
+
+/*Encapsulation example, Hiding Salary attribute and using get and set methods*/
+
+#include<iostream>
+using namespace std;
+
+class Employee
+{private:
+  int salary; 		  // private attribute
+ public:
+  void setSalary(int s)  // Setter 
+  {salary = s;           // set parameter s to private attribute salary
+  }
+  int getSalary() 	  // Getter
+  {return salary;        // return salary when getSalary() fun is called
+  }
+};
+
+int main()
+{Employee e1;		  //creating an object of Employee   
+ e1.setSalary(50000);    // set sallery 
+ cout<< "Salary of employee e1 is : " << e1.getSalary() << "\n";  //print
+ return 0;
+}
