036b99d0c543c040f7fbdd299d65ddf48bb7ea6b
swapnilcchougule <swapnilcchougule@gmail.com>
2022-10-05 12:41:03 +0200

    constructor code

--- 4.Constructor/par2_constructor.cpp
+++ 4.Constructor/par2_constructor.cpp
@@ -1,5 +1,8 @@
-/*constructor is special method which is invocked automatically at the time of object creation.
+/* Theory: https://www.geeksforgeeks.org/constructors-c/?ref=lbp
+constructor is special method which is invocked automatically at the time of object creation.
 constructor has same name as the class, followed by parentheses ()
+Constructor is invoked at the time of object creation. 
+It constructs the values i.e. provides data for the object which is why it is known as constructors.
 Types: 1. Default constructor (no arguments)  2. Paramaterized constructor
 Rules: 1.Same name as class
        2.no return type
@@ -30,8 +33,8 @@ Car::Car(string x, string y, int z)
  
 int main()
 {
- // Create Car objects and call the constructor with different values
- Car carObj1("BMW", "X5", 1999);
- Car carObj2("Ford", "Mustang", 1969);
+ // Create Car objects and it will invoke (call) the constructor (Car) 
+ Car carObj1("BMW", "X5", 1999);       // object1 created
+ Car carObj2("Ford", "Mustang", 1969); // object2 created
  return 0;
 }
--- 4.Constructor/par_constructor.cpp
+++ 4.Constructor/par_constructor.cpp
@@ -1,5 +1,8 @@
-/*constructor is special method which is invocked automatically at the time of object creation.
+/*Theory: https://www.geeksforgeeks.org/constructors-c/?ref=lbp
+constructor is special method which is invocked automatically at the time of object creation.
 constructor has same name as the class, followed by parentheses ()
+Constructor is invoked at the time of object creation. 
+It constructs the values i.e. provides data for the object which is why it is known as constructors.
 Types: 1. Default constructor (no arguments)  2. Paramaterized constructor
 Rules: 1.Same name as class
        2.no return type
