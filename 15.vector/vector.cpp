ae6c67b3de40f0eb6651bbc329ed005ce8d0c30d
swapnilcchougule <swapnilcchougule@gmail.com>
2022-10-23 22:17:27 +0200

    vector

---
+++ 15.vector/vector.cpp
@@ -0,0 +1,30 @@
+/*https://www.javatpoint.com/cpp-stl-components
+https://www.javatpoint.com/cpp-iterators
+Vector: vector is a class that creates a dynamic array allowing insertion and deletions at the back.
+	A vector is a sequence container class that implements dynamic array, means size automatically changes when appending elements. 
+	A vector stores the elements in contiguous memory locations and allocates the memory as needed at run time.
+Syntax: vector<object_type> v1;  
+
+
+Iterators : Iterators are just like pointers used to access the container elements.
+	    Iterators can be smart pointers which allow to iterate over the complex data structure
+Syntax    : <ContainerType> :: iterator;  
+	    <ContainerType> :: const_iterator;  	    
+*/
+
+#include<iostream>
+#include<iterator>
+#include<vector>
+
+using namespace std;
+
+int main()
+{vector<int> v1 {1,2,3,4,5};  // init vector v1
+ vector<int>::iterator itr;   // init iterator
+ for(itr=v1.begin();itr!=v1.end();itr++)  
+  {cout << *itr << "\n";  
+  }
+}
+
+
+
